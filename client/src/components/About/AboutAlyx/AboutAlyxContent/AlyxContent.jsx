90830e50df0a7f78b0ff3fffbab8c1f1d4c6620b
Lee.C32 <90383072+CuongXuanLe@users.noreply.github.com>
2022-04-18 23:04:18 +0700

    last update about

--- client/src/components/About/AboutAlyx/AboutAlyxContent/AlyxContent.jsx
+++ client/src/components/About/AboutAlyx/AboutAlyxContent/AlyxContent.jsx
@@ -4,7 +4,6 @@ import './AlyxContent.css'
 const AlyxContent = () => {
     return (
         <div className="alyx__content">
-
             <div className="card__container">
                 <div className="card__alyx-box">
                     <img src="https://www.sandbox.game/img/17_Sand/sandbox-logo-circle.svg" alt="circle" />
@@ -69,8 +68,8 @@ const AlyxContent = () => {
                         </li>
                     </ul>
                 </section>
-                <h2 className="key__metrics">Key metrics(as of January, 2022)</h2>
-                <div className="alyx__grid-table">
+            <h2 className="key__metrics">Key metrics(as of January, 2022)</h2>
+            <div className="alyx__grid-table">
                     <div className="header__grid-table ">
                         <p className="main__text-grid">Ticket</p>
                         </div>
@@ -83,6 +82,135 @@ const AlyxContent = () => {
                     <div className="sub__text-grid">0.0001238 UDS / ALYX</div>
                     <div className="sub__text-grid">Circulating Supply</div>
                     <div className="sub__text-grid">~20,000,000,000 ALYX</div>
+            </div>
+            <div className="whatisAlyx">
+                <div className="text">
+                    <h2>What is "The Alyx" (ALYX)</h2>
+                    <p> 
+                        The Alyx is a virtual metaverse where players can build, own, 
+                        and monetize their gaming experiences on the Ethereum blockchain using the platform’s 
+                        utility token ALYX. Players can create digital assets in the form of Non-Fungible Tokens 
+                        (NFTs), upload them to the marketplace,and integrate them into games with Game Maker.
+                    </p>
+                    </div>
+                <figure className="center">
+                    <img src="https://www.sandbox.game/img/17_Sand/sand-concept.png" alt="" />
+                </figure>
+                </div>
+                <div className="paragraphs1 whatisAlyxP">
+                    <p>
+                        A LAND is a digital piece of real estate in The Sandbox metaverse that players can buy to build experiences on. Once a player owns a piece of LAND, they will be able to populate it with games and ASSETS. Multiple LANDS can be combined to form an ESTATE.<br/>
+                        A total of 166,464 LANDS will be available, contained in a map that will form The Sandbox metaverse.<br/>
+                        GEMS and CATALYSTS define the tier, scarcity, and attributes of ASSETS. CATALYSTS add empty sockets to the NFTs that can be filled with GEMS. The higher the quality of the CATALYST, the more sockets your ASSET will have.<br/>
+                        Users can monetize their time on the platform, by keeping 95% of the SAND revenue by:<br/>
+                    </p>
+                </div>
+                <div className="items__container">
+                    <div className="items">
+                        <img src="https://www.sandbox.game/img/17_Sand/star-icon.svg" alt="" />
+                        <div className="column">
+                            <h2 className="column__text">Selling ASSETS</h2>
+                            <p>Users can create and sell ASSETS on the marketplace as NFTs</p>
+                        </div>
+                    </div>
+                    <div className="items">
+                        <img src="https://www.sandbox.game/img/17_Sand/star-icon.svg" alt="" />
+                        <div className="column">
+                            <h2 className="column__text">Owning a LAND</h2>
+                            <p>Buying a LAND in one of the LAND sales.
+                                 Players can then rent them or populate them with content to increase the LAND's value.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="items">
+                        <img src="https://www.sandbox.game/img/17_Sand/star-icon.svg" alt="" />
+                        <div className="column">
+                            <h2 className="column__text">Building Games on the Game Maker</h2>
+                            <p>Build and monetize games using the Game Maker on the LANDS that are owned by players.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="paragraphs1">
+                    <p>
+                    Through the use of ERC-1776, The Sandbox allows platform users to initiate transactions on the Ethereum blockchain while 
+                    transaction gas fees are paid by the platform, optimizing overall user experience.
+                    50% of all SAND transactions collected from LAND sales, 
+                    Premium NFT sales, transaction fees and subscription services goes to the Foundation
+                    </p>
+                </div>
+                <figure className="center">
+                    <img src="https://www.sandbox.game/img/17_Sand/tokensand.png" alt="" />
+                </figure>
+                <div className="paragraphs1">
+                    <p className="whatisAlyxP">
+                        The Foundation supports the ecosystem of The Alyx, contributing to the growth 
+                        of awareness about NFTs, the Metaverse, and ALYX.
+                    </p>
+                </div>
+                <h3 class="subtitle">The Foundation is currently involved with</h3>
+                <div className="items__container last">
+                    <div className="items">
+                        <img src="https://www.sandbox.game/img/17_Sand/star-icon.svg" alt="" />
+                        <div className="row">
+                            <small>
+                                <h2>   
+                                    Managing the Game Makers Fund and Creators Fund, 
+                                    offering grants to incentivize high quality content and 
+                                    game production in The Sandbox.
+                                </h2>
+                            </small>
+                        </div>
+                    </div>
+                    <div className="items">
+                        <img src="https://www.sandbox.game/img/17_Sand/star-icon.svg" alt="" />
+                        <div className="row">
+                            <small>
+                                <h2>   
+                                Funding over 17 game projects, some of which have been unveiled to the public at https://www.sandbox.game/game/top
+                                </h2>
+                            </small>
+                        </div>
+                    </div>
+                    <div className="items">
+                        <img src="https://www.sandbox.game/img/17_Sand/star-icon.svg" alt="" />
+                        <div className="row">
+                            <small>
+                                <h2>   
+                                    Distributing grants to 100 artists to produce NFTs, most of which can be seen at http://www.sandbox.game/shop
+                                </h2>
+                            </small>
+                        </div>
+                    </div>
+                    <div className="items">
+                        <img src="https://www.sandbox.game/img/17_Sand/star-icon.svg" alt="" />
+                        <div className="row">
+                            <small>
+                                <h2>   
+                                Supporting local community managers to grow awareness around the globe about The Sandbox, NFTs and blockchain gaming via our Community Rewards and Ambassadors program https://medium.com/sandbox-game/the-sandbox-ambassador-program-4f00bb5c9c24
+                                </h2>
+                            </small>
+                        </div>
+                    </div>
+                    <div className="items">
+                        <img src="https://www.sandbox.game/img/17_Sand/star-icon.svg" alt="" />
+                        <div className="row">
+                            <small>
+                                <h2>   
+                                Sponsoring prizes for the various contests organized by The Sandbox team (VoxEdit Contests and Game Jams) to stimulate the creativity of users around the world and get them to engage with the tools of our platform.
+                                </h2>
+                            </small>
+                        </div>
+                    </div>
+                    <div className="items">
+                        <img src="https://www.sandbox.game/img/17_Sand/star-icon.svg" alt="" />
+                        <div className="row">
+                            <small>
+                                <h2>   
+                                Supporting play-to-earn tournaments and cross-gaming with activities that encourage the broader adoption of SAND.
+                                </h2>
+                            </small>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
