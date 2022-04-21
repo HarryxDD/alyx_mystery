67706a44ac31f763327ee77471e0d9252cd3b6f8
Dân Thế Nguyễn <dan.nguyen200206@vnuk.edu.vn>
2022-04-22 00:16:53 +0700

    changed images.js in constants folder

--- client/src/constants/images.js
+++ client/src/constants/images.js
@@ -9,6 +9,19 @@ import arrowup from '../assets/arrowup.svg'
 import alyxwhite from '../assets/alyxwhite.svg'
 import about_banner from '../assets/about_banner.jpg'
 import lastest_post from '../assets/lastestpost.jpg'
+import arcanine from '../assets/arcanine.png'
+import lizardon from '../assets/charizard-megax.gif'
+import attack from '../assets/attack.png'
+import defense from '../assets/defense.png'
+import speed from '../assets/speed.png'
+import bullet from '../assets/bullet-icon.svg'
+import amogus from '../assets/amogus.jfif'
+import jirachi from '../assets/jirachi.png'
+import shuckle from '../assets/shuckle.png'
+import volcarona from '../assets/volcarona.png'
+import wobuffet from '../assets/wobuffet.png'
+import karp_shiny from '../assets/magikarp-shiny.png'
+import dragoon from '../assets/magmadragoon.png'
 
 import cuong from '../assets/IMG_20220420_093826.jpg'
 
@@ -33,5 +46,18 @@ export default {
     cuong,
     dan,
     nhut,
-    vu
+    vu,
+    lizardon,
+    attack,
+    defense,
+    speed,
+    bullet,
+    amogus,
+    jirachi,
+    shuckle,
+    volcarona,
+    wobuffet,
+    karp_shiny,
+    dragoon,
+    arcanine
 };
